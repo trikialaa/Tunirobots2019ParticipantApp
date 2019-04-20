50792a78be93f2417d41677706f50345cb4f8151
trikialaa <36111710+trikialaa@users.noreply.github.com>
2019-04-20 12:09:07 +0100

    Added menu

--- app/src/main/java/com/tunirobots/tunirobots/MainActivity.java
+++ app/src/main/java/com/tunirobots/tunirobots/MainActivity.java
@@ -13,12 +13,16 @@ import com.tunirobots.tunirobots.Features.FollowedTeams.Team;
 import com.tunirobots.tunirobots.Features.InsatMap.MapFragment;
 import com.tunirobots.tunirobots.Features.Matches.MatchsFragment;
 import com.tunirobots.tunirobots.Utils.SharedPreferencesUtils;
+import com.volcaniccoder.bottomify.BottomifyNavigationView;
+import com.volcaniccoder.bottomify.OnNavigationItemChangeListener;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Fragments
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
         if (findViewById(R.id.fragment)!=null){
             if (savedInstanceState!=null){
                 return;
@@ -52,6 +56,41 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.add(R.id.fragment, new ContactFragment(),null);
             fragmentTransaction.commit();
         }
+
+        // Bottom menu
+
+        BottomifyNavigationView bottomify = findViewById(R.id.bottomify_nav);
+         bottomify.setOnNavigationItemChangedListener(new OnNavigationItemChangeListener() {
+             @Override
+             public void onNavigationItemChanged(@NotNull BottomifyNavigationView.NavigationItem navigationItem) {
+                 String title = navigationItem.getTextView().getText().toString();
+                 if (title.equals("Équipes")){
+                     if (findViewById(R.id.fragment)!=null){
+                         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                         fragmentTransaction.replace(R.id.fragment, new FollowedTeamsFragment(),null);
+                         fragmentTransaction.commit();
+                     }
+                 } else if (title.equals("Matchs")){
+                     if (findViewById(R.id.fragment)!=null){
+                         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                         fragmentTransaction.replace(R.id.fragment, new MatchsFragment(),null);
+                         fragmentTransaction.commit();
+                     }
+                 } else if (title.equals("Carte")){
+                     if (findViewById(R.id.fragment)!=null){
+                         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                         fragmentTransaction.replace(R.id.fragment, new MapFragment(),null);
+                         fragmentTransaction.commit();
+                     }
+                 } else if (title.equals("Contact")){
+                     if (findViewById(R.id.fragment)!=null){
+                         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                         fragmentTransaction.replace(R.id.fragment, new ContactFragment(),null);
+                         fragmentTransaction.commit();
+                     }
+                 }
+             }
+         });
 
 
     }
